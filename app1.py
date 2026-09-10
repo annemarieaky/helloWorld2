3975f4f34e86497b285bdb02701e6b8119b71248
annemarieaky <aakyeam1@terpmail.umd.edu>
2026-09-10 13:17:21 -0400

    Added new route for hello HTML page.

--- app1.py
+++ app1.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Anne-Marie Akyeampong!'
+@app.route('hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
